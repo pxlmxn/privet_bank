42c984a47741878f2a818556bac6f742cf4a0b24
pxlmxn <124466865+pxlmxn@users.noreply.github.com>
2024-04-05 09:12:58 +0300

    Update models.py

--- bank_analyzer/models.py
+++ bank_analyzer/models.py
@@ -4,4 +4,4 @@ class Transaction(models.Model):
     card_id = models.IntegerField(verbose_name = 'id карты')
     amount = models.IntegerField(verbose_name = 'сумма')
     shop = models.CharField(max_length = 100, verbose_name = 'название магазина')
-    dt = models.DateTimeField(blank = True, null = True, verbose_name = 'дата и время платежа')
+    date = models.DateTimeField(blank = True, null = True, verbose_name = 'дата и время платежа')
